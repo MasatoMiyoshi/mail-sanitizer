5c06c5d59d237ffb95a5fac74bb6419593ab12ee
MasatoMiyoshi <miyoshi@sitebridge.co.jp>
2022-02-04 14:06:46 +0900

    Add quot pattern

--- spec/mail/sanitizer/sanitizer_spec.rb
+++ spec/mail/sanitizer/sanitizer_spec.rb
@@ -19,7 +19,7 @@ RSpec.describe Mail::Sanitizer do
   describe 'sanitize' do
     context 'when bodies include quoations' do
       let(:fixture_dir) { 'spec/fixtures/quot' }
-      it_behaves_like 'sanitizing bodies', 12
+      it_behaves_like 'sanitizing bodies', 13
     end
 
     context 'when bodies include signatures' do
